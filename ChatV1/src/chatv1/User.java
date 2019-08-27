cb14ba59eb41d190a3b69b2390f313708182a638
xOlive24 <hmpf802@gmail.com>
2019-08-26 19:54:16 -0700

    DUUUUUUUUUUUUUDE!!!!

--- ChatV1/src/chatv1/User.java
+++ ChatV1/src/chatv1/User.java
@@ -1,23 +1,26 @@
 package chatv1;
-
 public class User {
     String username,password,alias;
-    
+    Ventana in;
     public User(String username,String password,String alias){
         this.alias=alias;
         this.password = password;
         this.username=username;
     }
-    public void ChangeAlias(){
+    public String ChangeAlias(){
         
+        return alias;
     }
     public String GetUsername(){
+        username = in.userTF.getText();
         return username;
     }
     public String GetPassword(){
+        password = in.passTF.getText();
         return password;
     }
     public String GetAlias(){
+        alias = in.AliasTF.getText();
         return alias;
     }
 }
