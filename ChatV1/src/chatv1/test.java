21b39429d36fb19663528631637aa8624eb63723
dii <dii@05G205-PC9.uson.mx>
2019-08-30 07:13:36 -0700

    puto

--- ChatV1/src/chatv1/test.java
+++ ChatV1/src/chatv1/test.java
@@ -1,47 +1,46 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package chatv1;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.util.Random;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
-import javax.swing.SwingConstants;
 
-/**
- *
- * @author dii
- */
 public class test {
-
-    /**
-     * @param args the command line arguments
-     */
+    static Random rng = new Random();
+    
     public static void main(String[] args) {
-
-        ImageIcon BG = new ImageIcon("D:\\ChatV1\\ChatV1\\src\\resources\\MineBackground.jpg");
+        Color c = new Color(1f, 1f, 1f, .2f);
+        Color b = new Color(1f, 1f, 1f, .5f);
+        ImageIcon BG = new ImageIcon("D:\\ChatV1\\ChatV1\\src\\resources\\chat"+rng.nextInt(10)+".jpg");
         JFrame frame = new JFrame("Simple GUI");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         JLabel textLabel = new JLabel();
-        textLabel.setPreferredSize(new Dimension(720, 480));
+        textLabel.setPreferredSize(new Dimension(720, 405));
         textLabel.setIcon(BG);
+        textLabel.setOpaque(true);
+        
         frame.add(textLabel);
-        frame.getContentPane().add(textLabel, BorderLayout.CENTER);
-        frame.setLocationRelativeTo(null);
+        frame.getContentPane().add(textLabel);
         frame.pack();
-        JTextArea field1 = new JTextArea("asdsdasdasdasdasd");
-
-        Color c = new Color(1f, 1f, 1f, .2f);
-        field1.setBackground(c);
-        field1.setForeground(null);
-        frame.add(field1);
+        
+        JTextArea area1 = new JTextArea();
+        area1.setBackground(c);
+        area1.setEditable(false);
+        area1.setBounds(20,20, 600,300);
+        area1.setEditable(false);
+        
+        JTextField input = new JTextField();
+        input.setBackground(b);
+        input.setBounds(20,330, 600,50);
+        
+        
+        frame.add(area1);
+        frame.add(input);
+        
         frame.setVisible(true);
 
     }
